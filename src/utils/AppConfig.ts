5fab4681dda421bc0fc5b46fc380ed5e70b2f1b4
Peter Vincent <39063789+dev4vin@users.noreply.github.com>
2022-06-28 13:58:02 +0300

    start point

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Portfolio 1576',
+  title: 'Portfolio 1576 : Peter Vincent',
+  description: 'Personal portfolio containing projects I have done',
+  locale: 'en',
+};
